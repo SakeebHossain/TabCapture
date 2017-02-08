38809b47f7243c190891479a672622187e95d711
Sakeeb <sakeeb.hossain1@gmail.com>
2017-02-07 21:40:08 -0500

    redesigned interface a bit

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
-function saveCurrentTabs() {
-  
+function load() {
+	console.log("load");
 }
+
+function remove() {
+	console.log("remove");
+}
+
+function save() {
+	console.log("save");
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#load-button').addEventListener('click', load);
+  document.querySelector('#remove-button').addEventListener('click', remove);
+  document.querySelector('#save-button').addEventListener('click', save);
+});
